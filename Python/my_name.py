da579255c44b4133c78410ecb23abb1d7e4dafc6
Bla880 <brianda.lopez2@gmail.com>
2022-10-11 11:11:52 -0400

    Adding ~/PFB_problemsets/Python

--- Python/my_name.py
+++ Python/my_name.py
@@ -1,2 +1,5 @@
 #!/usr/bin/env python3
 print('My name:Brianda')
+print('My favorite color:Baby blue')
+print('My favorite activity:Dancing')
+print('My favorite animal:Axolotl')
---
+++ Python/my_name_sys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import sys
+my_name = sys.argv[1] # get first command line parameter
+my_color = sys.argv[2] # get second command line parameter
+my_activity = sys.argv[3] # get second command line parameter
+my_animal = sys.argv[4] # get second command line parameter
+# now print a message to the screen
+print('My name is:',my_name,'\n'+'My favorite color is:',my_color,'\n'+'My favorite activity is:',my_activity,'\n'+'My favorite animal is:',my_animal)
+
