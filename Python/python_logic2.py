1d6527123e15f232d3d1994f85e104d86804eba0
Bla880 <brianda.lopez2@gmail.com>
2022-10-11 16:39:50 -0400

    Adding python_logic2.py

--- Python/python_logic2.py
+++ Python/python_logic2.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 import sys
-count = 0
-if count < 0:
-  print('negative')
-elif count > 0:
-  print('positive')
+count = 10
+if count > 0:
+  message = 'positive'
+  print('the number is',message)
+elif count < 50:
+  message = 'smaller than 50'
+  print(message)
+elif (count > 0) and (count < 50):
+  message = 'smaller than 50'
+  print('the number is positive and',message)
+elif (count == 50) and (count < 50):
+  message = 'smaller than 50'
+  print('even and',message)
 else:
-  print('the number is zero')
+  message = 'The number is zero'
+  print(message)
