668133544f6b885d94ffafd5293db390c09b3b0b
Bla880 <brianda.lopez2@gmail.com>
2022-10-11 20:44:37 -0400

    Adding python_logic3.py

--- Python/python_logic3.py
+++ Python/python_logic3.py
@@ -2,34 +2,34 @@
 import sys
 count = sys.argv[1]
 
-if count < 0:
+if int(count) < 0:
   message = ' is negative'
   print (count,message)
-if count > 0:
+if int(count) > 0:
   message = ' is positive'
   print(count,message)
-if count < 50:
+if int(count) < 50:
   message = ' is smaller than 50'
   print(count,message)
-  if count %2 == 0:
+  if int(count) %2 == 0:
    message = ' is an even number'
    print(count,message)
 else:
-  if count %2 == 0:
+  if int(count) %2 == 0:
    message = ' is an even number'
    print(count,message)
-if count > 50:
+if int(count) > 50:
   message = '  is larger than 50'
   print(count,message)
-  if count %3 == 0:
+  if int(count) %3 == 0:
    message = ' is divisible by 3'
    print(count,message)
-if count %3 == 0:
+if int(count) %3 == 0:
    message = ' is divisible by 3'
    print(count,message)
-if count == 50:
+if int(count) == 50:
   message = ' is 50'
   print(count,message)
-if count == 0:
+if int(count) == 0:
   message = ' is zero'
   print(count,message)
