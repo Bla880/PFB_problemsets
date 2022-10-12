4305f1f08dc29fbf70bc6fea3608c57686ca5216
Bla880 <brianda.lopez2@gmail.com>
2022-10-11 20:29:42 -0400

    Adding python_logic2.py

--- Python/python_logic2.py
+++ Python/python_logic2.py
@@ -1,18 +1,32 @@
 #!/usr/bin/env python3
 import sys
 count = 10
+if count < 0:
+  print ('negative')
 if count > 0:
   message = 'positive'
   print('the number is',message)
-elif count < 50:
-  message = 'smaller than 50'
+if count < 50:
+  message = 'this number is smaller than 50'
   print(message)
-elif (count > 0) and (count < 50):
-  message = 'smaller than 50'
-  print('the number is positive and',message)
-elif (count == 50) and (count < 50):
-  message = 'smaller than 50'
-  print('even and',message)
+  if count %2 == 0:
+   message = 'Even number'
+   print(message)
 else:
+  if count %2 == 0:
+    print('Even number')
+if count > 50:
+  message = 'this number is larger than 50'
+  print(message)
+  if count %3 == 0:
+   message = 'and divisible by 3'
+   print(message)
+if count %3 == 0:
+   message = 'this number is divisible by 3'
+   print(message)
+if count == 50:
+  message = 'The number is 50'
+  print(message)
+if count == 0:
   message = 'The number is zero'
   print(message)
